063d0243051b896c98ef1c4116718743f6712d01
Allam9573 <aallamdaniel@gmail.com>
2023-06-01 10:26:15 -0600

    metodos en operar lista de canciones

--- src/src/Main.java
+++ src/src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Cancion> listaCanciones = new ArrayList<>();
-        Cancion c1 = new Cancion(0, "Dont Cry", 0, "Guns and Roses");
+        Cancion c1 = new Cancion(1, "Dont Cry", 1985, "Guns and Roses");
         listaCanciones.add(c1);
-        System.out.println(listaCanciones.get(0).getNombre() +" - "+listaCanciones.get(0).getArtista());
+        System.out.println(listaCanciones.get(0).getNombre() + " - " + listaCanciones.get(0).getArtista());
     }
 }
--- src/src/Operaciones.java
+++ src/src/Operaciones.java
@@ -4,12 +4,31 @@
  */
 package src;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lukanka
  */
 public class Operaciones {
-    
-    
-    
+
+    private ArrayList<Cancion> listaCanciones;
+
+    public ArrayList<Cancion> getListaCanciones() {
+        return listaCanciones;
+    }
+
+    public void setListaCanciones(Cancion cancion) {
+        listaCanciones.add(cancion);
+    }
+
+    public void buscarCancion(Cancion cancion) {
+        for (int i = 0; i < listaCanciones.size(); i++) {
+            if (listaCanciones.get(i).getNombre() == cancion.getNombre()) {
+                System.out.println("Si existe la cancion");
+            } else {
+                System.out.println("No existe la cancion");
+            }
+        }
+    }
 }
